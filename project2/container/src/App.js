0f99162234c061fcf115b2ac30f536201ec39529
Mark Astin <redwolf89@gmail.com>
2020-11-23 16:23:45 -0500

    trying to get the workflow to run2

--- project2/container/src/App.js
+++ project2/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there buddy</h1>
+      <h1>Hi there buddroo</h1>
       <hr />
       <MarketingApp />
     </div>
